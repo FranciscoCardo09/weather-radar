ab9e1bf06b8295fe257690984287b498d5e76b42
FranciscoCardo09 <pnchocardo@gmail.com>
2026-02-06 12:51:17 -0300

    Arreglo de versiones de tailwind. Paso de V3 a V4

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import { getCities, compareWeather, getWeatherForCity } from '$lib/api';
+    import type { Cities, WeatherData } from '$lib/types';
+    
+    let cities: Cities[] = [];
+    let cityId1: string = '';
+    let cityId2: string = '';
+    let comparisonResult: any = null;
+    let weatherCity1: WeatherData | null = null;
+    let weatherCity2: WeatherData | null = null;
+    let errorMessage: string = '';
+    
+    onMount(async () => {
+        cities = await getCities();
+    });
+    
+    async function compare() {
+        errorMessage = '';
+        comparisonResult = null;
+        weatherCity1 = null;
+        weatherCity2 = null;
+    
+        try {
+        comparisonResult = await compareWeather(cityId1, cityId2);
+        weatherCity1 = await getWeatherForCity(cityId1);
+        weatherCity2 = await getWeatherForCity(cityId2);
+        } catch (error) {
+        errorMessage = 'Error al obtener los datos del clima.';
+        }
+    }
+</script>
+<main>
+    
+</main>
